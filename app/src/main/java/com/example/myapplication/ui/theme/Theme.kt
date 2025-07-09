8af36d86833374917c022cf7e6e9e7ab7babe61b
clyde <clydeisavoter@gmail.com>
2025-07-09 04:16:01 -0500

    change background color?

--- app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
@@ -23,7 +23,7 @@ private val LightColorScheme = lightColorScheme(
     tertiary = Pink40
 
     /* Other default colors to override
-    background = Color(0xFFFFFBFE),
+    background = Color(0x928c8c),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
     onSecondary = Color.White,
