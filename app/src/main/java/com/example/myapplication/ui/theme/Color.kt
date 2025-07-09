e14c45d6525062cbe4024339ff98703568dc3138
clyde <clydeisavoter@gmail.com>
2025-07-09 04:10:58 -0500

    finished the foreground updates and app is stable

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -6,6 +6,6 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val Purple40 = Color(0xFF2336C6)
+val PurpleGrey40 = Color(0xFF1180A0)
 val Pink40 = Color(0xFF7D5260)
